e5e6b1bf7b57493df54f10b8a4959b0c576dd77e
Mistyckary <167718301+Mistyckary@users.noreply.github.com>
2024-07-06 13:57:42 -0300

    Update contact.js

--- app/contact.js
+++ app/contact.js
@@ -1,1 +1,16 @@
+const toastLiveExample = document.getElementById('liveToast');
 
+function btnSendMail() {
+  
+  let inputName = document.querySelector('#contact-name').value;
+  let inputMail = document.querySelector('#contact-mail').value;
+  let inputSubject = document.querySelector('#contact-subject').value;
+  let inputMessage = document.querySelector('#contact-message').value;
+
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+
+  if(inputName.value !== "" && inputMail !== "" && inputSubject !== "" && inputMessage !== ""){
+    toastBootstrap.show();
+
+  }
+}
